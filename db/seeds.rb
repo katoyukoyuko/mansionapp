7dc8d4fe66c25f95d1ebf7f39012ea0bda7756ef
kato yuko <yy_yuko_121@yahoo.co.jp>
2020-05-19 18:11:21 +0900

    seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+    name: '日本花子',
+    email: 'hanako@test.com',
+    password: '000000',
+    password_confirmation: '000000',
+    role: 'chairman',
+    )
+
+BulletinBoard.create!(
+    title: '第1回理事会議事録',
+    content: open("#{Rails.root}/db/fixtures/note1.pdf"),
+    memo: '2020年4月15日に開催した第1回理事会の議事録を作成いたしましたので、ご確認お願いいたします。',
+    user_id: 1,
+    )
